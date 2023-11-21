21de468f8e824547a617999b30b3400459771ae1
Your NameDany <you@example.comdanydc1@hotmail.fr>
2023-11-21 10:55:14 +0100

    add audit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
 <header role="banner">
     <h1>Bienvenue sur mon devoir de développement durable</h1>
 </header>
-<p>Cliquez sur une question pour voir la réponse.</p>
+
+<h2>Pour info, ce site web est développé selon les bonnes pratiques du green IT : </h2>
+<p>(Cliquez sur une question pour voir la réponse.)</p>
+<img src="/img/audit.png" alt="screenshot audit lighthouse">
+<img src="/img/ecoindexe.png" alt="screenshot audit ecoindexe">
+
+
+<style>
+    img{
+        border-radius: 15px;
+        box-sizing: border-box;
+        margin:10px;
+        width:450px
+    }
+</style>
